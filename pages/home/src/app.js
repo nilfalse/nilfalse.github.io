077d67cf1d4297148b743abee66b7d919880d2a9
Yaroslav Ilin <nilfalse@yandex.com>
2021-04-03 13:22:57 +0200

    Paint text rainbow

--- pages/home/src/app.js
+++ pages/home/src/app.js
@@ -4,7 +4,10 @@ export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="App-hello">Hello, World 👋</p>
+        <p className="App-hello">
+          <span className="App-hello-rainbow">Hello, World</span>{' '}
+          <span className="App-hello-emoji">👋</span>
+        </p>
       </header>
     </div>
   );
